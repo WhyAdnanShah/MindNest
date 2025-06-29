4fd7282314b8a4c9957203f7f416648ff672ea59
Mohd Adnan <mohdadnan23742@gmail.com>
2025-06-29 09:18:19 +0530

    .

--- app/src/main/java/com/example/mentalhealthapp/destinations/MoodScreen.kt
+++ app/src/main/java/com/example/mentalhealthapp/destinations/MoodScreen.kt
@@ -2,6 +2,7 @@ package com.example.mentalhealthapp.destinations
 
 import android.annotation.SuppressLint
 import android.util.Log
+import android.widget.Toast
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -31,6 +32,7 @@ import com.example.mentalhealthapp.viewModel.MoodViewModel
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import com.example.mentalhealthapp.moodScreen.MoodDialog
 import com.example.mentalhealthapp.moodScreen.MoodItemCard
--- app/src/main/java/com/example/mentalhealthapp/moodScreen/MoodItemCard.kt
+++ app/src/main/java/com/example/mentalhealthapp/moodScreen/MoodItemCard.kt
@@ -1,5 +1,6 @@
 package com.example.mentalhealthapp.moodScreen
 
+import android.widget.Toast
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
@@ -13,6 +14,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
@@ -20,14 +23,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.mentalhealthapp.R
 import com.example.mentalhealthapp.database.MoodEntity
 
 @Composable
 fun MoodItemCard(moods: MoodEntity){
+    val context = LocalContext.current
     Card(
         modifier = Modifier
             .fillMaxWidth()
@@ -91,6 +97,26 @@ fun MoodItemCard(moods: MoodEntity){
             Text(
                 text = moods.note,
             )
+            Spacer(Modifier.height(10.dp))
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.End
+            ){
+                Button (modifier = Modifier
+                    .width(100.dp)
+                    .height(35.dp),
+                    onClick = { Toast.makeText(context, "Delete id: " + moods.id, Toast.LENGTH_SHORT).show()},
+                    colors = ButtonDefaults.buttonColors(colorResource(R.color.light_red))
+                ){
+                    Text(text = "Delete",
+                        fontSize = 13.sp
+                    )
+                }
+            }
+
         }
 
     }
