476846d37b38e65f53039e5890a25592394af113
Mohd Adnan <mohdadnan23742@gmail.com>
2025-06-30 06:08:42 +0530

    .

--- app/src/main/java/com/example/mentalhealthapp/moodScreen/MoodItemCard.kt
+++ app/src/main/java/com/example/mentalhealthapp/moodScreen/MoodItemCard.kt
@@ -1,6 +1,5 @@
 package com.example.mentalhealthapp.moodScreen
 
-import android.widget.Toast
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
@@ -14,25 +13,16 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.mentalhealthapp.R
 import com.example.mentalhealthapp.database.MoodEntity
 import com.example.mentalhealthapp.viewModel.MoodViewModel
@@ -126,8 +116,3 @@ fun MoodItemCard(moods: MoodEntity, moodViewModel: MoodViewModel){
         }
     }
 }
-
-
-
-
-
