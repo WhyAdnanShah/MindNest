54cbda829d079c7fc57bcede0aa6518860d6d80c
Mohd Adnan <mohdadnan23742@gmail.com>
2025-06-30 21:04:26 +0530

    .

--- app/src/main/java/com/example/mentalhealthapp/moodScreen/MoodDialog.kt
+++ app/src/main/java/com/example/mentalhealthapp/moodScreen/MoodDialog.kt
@@ -1,7 +1,5 @@
 package com.example.mentalhealthapp.moodScreen
 
-import android.util.Log
-import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -10,11 +8,13 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -38,7 +38,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontStyle
@@ -68,18 +67,19 @@ fun MoodDialog(onDismiss: () -> Unit, moodViewModel: MoodViewModel) {
     Dialog(onDismissRequest = onDismiss) {
         Card(
             modifier = Modifier
-                .fillMaxWidth()
-                .height(500.dp)
+                .wrapContentSize()
         ) {
             Column(
                 modifier = Modifier
-                    .fillMaxSize()
+                    .wrapContentSize()
                     .padding(16.dp),
                 horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.SpaceEvenly
+                verticalArrangement = Arrangement.Top
             ) {
                 TitleText(text = "How are you feeling right now?")
 
+                Spacer(modifier = Modifier.height(25.dp))
+                
                 val images = listOf(
                     R.drawable.laughing,
                     R.drawable.smiling,
@@ -93,6 +93,8 @@ fun MoodDialog(onDismiss: () -> Unit, moodViewModel: MoodViewModel) {
                     onSelected = { selected = it }
                 )
 
+                Spacer(modifier = Modifier.height(25.dp))
+
                 NoteField(
                     value = moodNote,
                     onValueChange = { moodNote = it },
@@ -100,6 +102,8 @@ fun MoodDialog(onDismiss: () -> Unit, moodViewModel: MoodViewModel) {
                     placeholderText = "Enter Note",
                 )
 
+                Spacer(modifier = Modifier.height(25.dp))
+                
                 Card(
                     modifier = Modifier
                         .fillMaxWidth()
@@ -122,6 +126,8 @@ fun MoodDialog(onDismiss: () -> Unit, moodViewModel: MoodViewModel) {
                         onDismiss = { datePickerCard = false }
                     )
                 }
+
+                Spacer(modifier = Modifier.height(25.dp))
 
                 Row(
                     modifier = Modifier.fillMaxWidth(),
--- app/src/main/java/com/example/mentalhealthapp/moodScreen/MoodEdit.kt
+++ app/src/main/java/com/example/mentalhealthapp/moodScreen/MoodEdit.kt
@@ -1,31 +1,56 @@
 package com.example.mentalhealthapp.moodScreen
 
+import android.R.attr.title
 import android.widget.Toast
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
 import com.example.mentalhealthapp.R
 import com.example.mentalhealthapp.database.MoodEntity
 import com.example.mentalhealthapp.viewModel.MoodViewModel
+import java.text.SimpleDateFormat
+import java.util.Locale
+import kotlin.collections.listOf
 
 @Composable
 fun MoodEdit(moodViewModel: MoodViewModel, moods: MoodEntity) {
@@ -63,36 +88,144 @@ fun MoodEdit(moodViewModel: MoodViewModel, moods: MoodEntity) {
         moodViewModel = moodViewModel,
         moods = moods
     )
-
-
 }
 
 @Composable
-fun EditMoodDialog(onDismiss: () -> Unit, moodViewModel: MoodViewModel, moods: MoodEntity) {
-    AlertDialog(
-        onDismissRequest = onDismiss,
-        title = { Text("Edit Mood Entry") },
-        text = { Text("Are you sure you want to edit this mood entry?") },
-        confirmButton = {
-            Button(
-                onClick = {
-                    moodViewModel.editMood(moods)
-                    onDismiss()
-                }
-            ) {
-                Text("Confirm")
-            }
-        },
-        dismissButton = {
-            Button(
-                onClick = onDismiss,
-                colors = ButtonDefaults.buttonColors(Color.Transparent)
-            ) {
-                Text("Cancel",
-                    color = colorResource(R.color.light_red)
+fun EditMoodDialog(
+    onDismiss: () -> Unit,
+    moodViewModel: MoodViewModel,
+    moods: MoodEntity
+) {
+    var selected by remember { mutableIntStateOf(0) }
+    var moodNoteEdit by remember { mutableStateOf("") }
+
+    val images = listOf(
+        R.drawable.laughing,
+        R.drawable.smiling,
+        R.drawable.neutral,
+        R.drawable.sad,
+        R.drawable.dead
+    )
+    Dialog(onDismissRequest = onDismiss)
+    {
+        Card(modifier = Modifier
+            .wrapContentSize()
+            .fillMaxWidth()
+        ){
+            Column (modifier = Modifier
+                .wrapContentSize()
+                .padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceEvenly
+
+            ){
+                TitleText(text = "Edit Mood Entry")
+
+                Spacer(modifier = Modifier.height(25.dp))
+
+                ImageGroup(
+                    imageResIds = images,
+                    selectedIndex = selected,
+                    onSelected = { selected = it }
                 )
+
+                Spacer(modifier = Modifier.height(25.dp))
+
+                EditNoteField(
+                    value = moodNoteEdit,
+                    onValueChange = { moodNoteEdit = it },
+                    labelText = "What affected your mood?",
+                    placeholderText = "Enter Note",
+                )
+
+                Spacer(modifier = Modifier.height(25.dp))
+
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.End
+                ){
+                    Button(
+                        onClick = onDismiss,
+                        colors = ButtonDefaults.buttonColors(Color.Transparent)
+                    ) {
+                        Text(
+                            "Cancel",
+                            color = colorResource(R.color.light_red)
+                        )
+                    }
+
+                    Button(
+                        onClick = {
+                            moodViewModel.editMood(
+                                MoodEntity(
+                                    mood = images[selected].toString(),
+                                    note = moodNoteEdit,
+                                ))
+                            onDismiss()
+                        }
+                    ) {
+                        Text("Confirm")
+                    }
+                }
+
             }
 
         }
+    }
+}
+
+@Composable
+fun EditNoteField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    labelText: String,
+    placeholderText: String
+) {
+    OutlinedTextField(
+        modifier = Modifier
+            .fillMaxWidth(),
+        value = value,
+        onValueChange = onValueChange,
+        label = { Text(labelText) },
+        placeholder = { Text(placeholderText) },
+        shape = RoundedCornerShape(15.dp),
+        singleLine = false,
+        maxLines = 10
     )
+}
+
+
+@Composable
+fun ImageGroup(imageResIds: List<Int>, selectedIndex: Int, onSelected: (Int) -> Unit) {
+    Row {
+        imageResIds.forEachIndexed { index, resId ->
+            val isSelected = index == selectedIndex
+            Box(
+                modifier = Modifier
+                    .padding(8.dp)
+                    .size(40.dp)
+                    .clip(RoundedCornerShape(12.dp))
+                    .border(
+                        width = if (isSelected) 1.dp else 1.dp,
+                        color = if (isSelected) colorResource(R.color.cornflower_blue) else Color.LightGray,
+                        shape = RoundedCornerShape(12.dp)
+                    )
+                    .clickable { onSelected(index) }
+            ) {
+                Image(
+                    painter = painterResource(id = resId),
+                    contentDescription = "Option $index",
+                    modifier = Modifier.fillMaxSize(),
+                    contentScale = ContentScale.Crop
+                )
+                if (isSelected) {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(Color(0x330000FF))
+                    )
+                }
+            }
+        }
+    }
 }
