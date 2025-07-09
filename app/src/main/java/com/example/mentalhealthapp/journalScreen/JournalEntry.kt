57893017b4182110c838e487d84f4805ed6fa242
Mohd Adnan <mohdadnan23742@gmail.com>
2025-07-09 15:15:43 +0530

    .

--- app/src/main/java/com/example/mentalhealthapp/journalScreen/JournalEntry.kt
+++ app/src/main/java/com/example/mentalhealthapp/journalScreen/JournalEntry.kt
@@ -11,15 +11,16 @@ import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -30,6 +31,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -45,87 +47,84 @@ fun JournalEntry(navHostController: NavHostController) {
     var titleText by remember { mutableStateOf("") }
     var noteText by remember { mutableStateOf("") }
 
-    var imageUri by remember { mutableStateOf<Uri?>(null) }
+    var imageUri by remember { mutableStateOf<List<Uri?>>(emptyList()) }
 
 
     /*          This is a simple Media Picker         */
-    val pickMedia = rememberLauncherForActivityResult(ActivityResultContracts.PickVisualMedia()) {
-            uri ->
-        if (uri != null) {
-            imageUri= uri
+    val pickMedia = rememberLauncherForActivityResult(ActivityResultContracts.PickMultipleVisualMedia(5)) {
+            uris ->
+        if (uris.isNotEmpty()) {
+            imageUri = uris
             Toast.makeText(context, "Image Selected", Toast.LENGTH_SHORT).show()
         }
         else{
             Toast.makeText(context, "No Image Selected", Toast.LENGTH_SHORT).show()
-
         }
     }
 
 
     Column(
         modifier = Modifier
-            .fillMaxWidth()
-            .padding(16.dp)
-            .scrollable(
-                orientation = Orientation.Vertical,
-                state = rememberScrollState()
-            ),
+            .verticalScroll(rememberScrollState())
+            .wrapContentSize()
+            .padding(16.dp),
         horizontalAlignment = Alignment.Start,
         verticalArrangement = Arrangement.Top
     ) {
+//        item{
+            OutlinedTextField(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(70.dp),
+                value = titleText,
+                onValueChange = { titleText = it },
+                label = { Text("Title", fontSize = 20.sp) },
+                shape = RoundedCornerShape(15.dp),
+            )
+//        }
 
-        OutlinedTextField(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(70.dp),
-            value = titleText,
-            onValueChange = { titleText = it },
-            label = { Text("Title", fontSize = 20.sp) },
-            shape = RoundedCornerShape(15.dp),
-        )
+//        item{
+            OutlinedTextField(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(500.dp),
+                value = noteText,
+                onValueChange = { noteText = it },
+                label = { Text("Note") },
+                shape = RoundedCornerShape(15.dp)
+            )
+            Spacer(Modifier.height(16.dp))
 
-        Spacer(Modifier.height(10.dp))
+//        }
 
-        OutlinedTextField(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(500.dp),
-            value = noteText,
-            onValueChange = { noteText = it },
-            label = { Text("Note") },
-            shape = RoundedCornerShape(15.dp)
-        )
-
-        Spacer(Modifier.height(10.dp))
-
-        Button(modifier = Modifier.wrapContentSize(),
-            onClick = {
-                pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+//        item{
+            Button(modifier = Modifier.wrapContentSize(),
+                onClick = {
+                    pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+                }
+            ) {
+                Text(text = "Select Image")
             }
-        ) {
-            Text(text = "Select Image")
-        }
+            Spacer(Modifier.height(16.dp))
+//        }
 
-        Text(text = "Image Preview", fontSize = 20.sp)
+//        item {
+            SelectedImages(imageUri as Uri?
+            )
+//        }
+    }
+}
 
-        Row (
+@Composable
+fun SelectedImages(imageUri: Uri?) {
+    Text(text = "Image Preview", fontSize = 20.sp)
+
+    imageUri?.let{
+            uris ->
+        Image(
             modifier = Modifier
-                .fillMaxWidth()
-                .height(500.dp),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.Start
-        ){
-
-            imageUri?.let{
-                    uri ->
-                Image(
-                    modifier = Modifier
-                        .width(200.dp).height(300.dp),
-//                        .clip(RoundedCornerShape(10.dp)),
-                    painter = rememberAsyncImagePainter(uri), contentDescription = null)
-            }
-        }
-
-
+                .size(300.dp)
+                .clip(RoundedCornerShape(10.dp)),
+            painter = rememberAsyncImagePainter(uris), contentDescription = null)
     }
 }
