0aee371cff58125d2dd5517d84d1c09a12353aa3
Mohd Adnan <mohdadnan23742@gmail.com>
2025-07-12 20:39:44 +0530

    .

--- app/src/main/java/com/example/mentalhealthapp/journalScreen/JournalItemCard.kt
+++ app/src/main/java/com/example/mentalhealthapp/journalScreen/JournalItemCard.kt
@@ -2,21 +2,31 @@ package com.example.mentalhealthapp.journalScreen
 
 import android.util.Log
 import android.widget.Toast
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import com.example.mentalhealthapp.R
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ButtonDefaults.shape
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.unit.dp
@@ -60,6 +70,48 @@ fun JournalItemCard(journalEntity: JournalEntity, journalViewModel: JournalViewM
                 text = journalEntity.title,
                 fontSize = 17.sp
             )
+            Row(
+                Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.End,
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Button(
+                    modifier = Modifier
+                        .wrapContentSize()
+                        .border(
+                            width = 0.dp,
+                            color = colorResource(R.color.earth_yellow),
+                            shape = RoundedCornerShape(10.dp)
+                        ),
+                    onClick = {
+                        Toast.makeText(context, "Edit Clicked", Toast.LENGTH_SHORT).show()
+                              },
+                    shape = RoundedCornerShape(10.dp),
+                    colors = ButtonDefaults.buttonColors(Color.Transparent),
+                )
+                {
+                    Text("Edit", color = colorResource(R.color.earth_yellow))
+                }
+                Spacer(modifier = Modifier.width(10.dp))
+                Button(
+                    modifier = Modifier
+                        .wrapContentSize()
+                        .border(
+                            width = 0.dp,
+                            color = colorResource(R.color.light_red),
+                            shape = RoundedCornerShape(10.dp)
+                        ),
+                    onClick = {
+                        Toast.makeText(context, "Delete Clicked", Toast.LENGTH_SHORT).show()
+                        journalViewModel.removeJournal(journalEntity)
+                    },
+                    shape = RoundedCornerShape(10.dp),
+                    colors = ButtonDefaults.buttonColors(Color.Transparent),
+                )
+                {
+                    Text("Delete", color = colorResource(R.color.light_red))
+                }
+            }
         }
     }
 }
