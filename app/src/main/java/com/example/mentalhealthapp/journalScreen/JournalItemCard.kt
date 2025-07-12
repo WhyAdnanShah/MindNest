7f38613ba6f3699b01073e82e4144c00b2228eec
Mohd Adnan <mohdadnan23742@gmail.com>
2025-07-12 14:25:54 +0530

    .

--- app/src/main/java/com/example/mentalhealthapp/journalScreen/JournalItemCard.kt
+++ app/src/main/java/com/example/mentalhealthapp/journalScreen/JournalItemCard.kt
@@ -1,10 +1,24 @@
 package com.example.mentalhealthapp.journalScreen
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material3.Card
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.mentalhealthapp.journalROOMdatabase.JournalEntity
 import com.example.mentalhealthapp.viewModel.JournalViewModel
 
 @Composable
 fun JournalItemCard(journalEntity: JournalEntity, journalViewModel: JournalViewModel) {
+    Card (
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .padding(horizontal = 16.dp, vertical = 10.dp)
+    ){
+
+    }
 
 }
