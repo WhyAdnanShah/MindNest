501890be3a6ccc371914c4f56c9a3b5446f3de6f
Mohd Adnan <mohdadnan23742@gmail.com>
2025-07-19 01:26:20 +0530

    .

--- app/src/main/java/com/example/mentalhealthapp/destinations/ZenModeScreen.kt
+++ app/src/main/java/com/example/mentalhealthapp/destinations/ZenModeScreen.kt
@@ -3,16 +3,22 @@ package com.example.mentalhealthapp.destinations
 import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Info
 import androidx.compose.material3.Card
@@ -23,12 +29,15 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
+import com.example.mentalhealthapp.R
 
 
 @Composable
@@ -97,19 +106,38 @@ fun ZenModeScreen() {
 @Composable
 fun InfoDialog(onDismiss: () -> Unit, title: String) {
     val context = LocalContext.current
-    Dialog(
-        onDismissRequest = onDismiss
-    ) {
+    Dialog(onDismissRequest = onDismiss) {
         Card (
             Modifier
                 .fillMaxWidth()
-                .height(300.dp)
+                .fillMaxHeight()
+                .verticalScroll(rememberScrollState())
         ){
             Text(modifier = Modifier.padding(16.dp),
                 text = title,
                 fontSize = 25.sp,
                 fontWeight = FontWeight.Bold
             )
+            when (title) {
+                "Equal Breathing" -> {
+                    Text(modifier = Modifier.padding(16.dp),
+                        text = stringResource(R.string.Equal_Breathing),
+                        fontSize = 17.sp
+                    )
+                }
+                "Box Breathing" -> {
+                    Text(modifier = Modifier.padding(16.dp),
+                        text = stringResource(R.string.Box_Breathing),
+                        fontSize = 17.sp
+                    )
+                }
+                else -> {
+                    Text(modifier = Modifier.padding(16.dp),
+                        text = stringResource(R.string.four_seven_eight_Breathing),
+                        fontSize = 17.sp
+                    )
+                }
+            }
         }
     }
 }
