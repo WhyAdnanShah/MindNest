be53bf21aed0825da2e87a3ec3c6eb873c5927ec
Mohd Adnan <mohdadnan23742@gmail.com>
2025-07-19 00:05:41 +0530

    .

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.play.services.cast.framework)
+    implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/mentalhealthapp/destinations/ZenModeScreen.kt
+++ app/src/main/java/com/example/mentalhealthapp/destinations/ZenModeScreen.kt
@@ -1,10 +1,55 @@
 package com.example.mentalhealthapp.destinations
 
+import android.widget.Toast
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.mentalhealthapp.navigation.CenteredText
 
+
 @Composable
 fun ZenModeScreen() {
-    CenteredText("Feature Coming Soon", 20.sp)
+    val context = LocalContext.current
+
+    val items = listOf("Equal Breathing", "Box Breathing", "4-7-8 Breathing")
+
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        modifier = Modifier.height(200.dp),
+//        state = TODO(),
+        contentPadding = PaddingValues(16.dp),
+        verticalArrangement =Arrangement.spacedBy(16.dp),
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+    ){
+        items(items.size){
+            index ->
+            Card (
+                Modifier
+                    .size(200.dp,300.dp)
+                    .clickable(
+                        onClick = {
+                            Toast.makeText(context, items[index], Toast.LENGTH_SHORT).show()
+                        }
+                    ),
+            ){
+                CenteredText(items[index], fontSize = 20.sp)
+            }
+        }
+    }
 }
