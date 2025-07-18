52295a87b7a66131f7a8396cb34ac8e391ae5773
Mohd Adnan <mohdadnan23742@gmail.com>
2025-07-19 01:44:32 +0530

    .

--- app/src/main/java/com/example/mentalhealthapp/destinations/ZenModeScreen.kt
+++ app/src/main/java/com/example/mentalhealthapp/destinations/ZenModeScreen.kt
@@ -2,6 +2,7 @@ package com.example.mentalhealthapp.destinations
 
 import android.widget.Toast
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.gestures.scrollable
@@ -18,9 +19,12 @@ import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Info
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -31,7 +35,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -120,23 +126,39 @@ fun InfoDialog(onDismiss: () -> Unit, title: String) {
             )
             when (title) {
                 "Equal Breathing" -> {
-                    Text(modifier = Modifier.padding(16.dp),
+                    Text(modifier = Modifier.padding(horizontal = 16.dp),
                         text = stringResource(R.string.Equal_Breathing),
                         fontSize = 17.sp
                     )
                 }
                 "Box Breathing" -> {
-                    Text(modifier = Modifier.padding(16.dp),
+                    Text(modifier = Modifier.padding(horizontal = 16.dp),
                         text = stringResource(R.string.Box_Breathing),
                         fontSize = 17.sp
                     )
                 }
                 else -> {
-                    Text(modifier = Modifier.padding(16.dp),
+                    Text(modifier = Modifier.padding(horizontal = 16.dp),
                         text = stringResource(R.string.four_seven_eight_Breathing),
                         fontSize = 17.sp
                     )
                 }
+            }
+            Button(
+                modifier = Modifier
+                    .align(Alignment.End)
+                    .padding(16.dp)
+                    .border(
+                        width = 1.dp,
+                        color = colorResource(R.color.indian_red),
+                        shape = RoundedCornerShape(20.dp)
+                    ),
+                onClick = {
+                    onDismiss()
+                },
+                colors = ButtonDefaults.buttonColors(Color.Transparent)
+            ) {
+                Text("Dismiss", color = colorResource(R.color.indian_red))
             }
         }
     }
