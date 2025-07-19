df71f79eed85fecb555c1859b6acd3ed2681b746
Mohd Adnan <mohdadnan23742@gmail.com>
2025-07-20 02:19:30 +0530

    .

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,12 +8,12 @@ plugins {
 
 android {
     namespace = "com.example.mentalhealthapp"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.mentalhealthapp"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
