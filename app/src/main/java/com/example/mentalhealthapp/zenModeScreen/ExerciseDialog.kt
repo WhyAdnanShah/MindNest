bf46a4730b71b428e682f0ae4972d92008a06d73
Mohd Adnan <mohdadnan23742@gmail.com>
2025-07-19 21:10:26 +0530

    .

--- app/src/main/java/com/example/mentalhealthapp/zenModeScreen/ExerciseDialog.kt
+++ app/src/main/java/com/example/mentalhealthapp/zenModeScreen/ExerciseDialog.kt
@@ -8,13 +8,10 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.AssistChip
 import androidx.compose.material3.AssistChipDefaults
 import androidx.compose.material3.Button
