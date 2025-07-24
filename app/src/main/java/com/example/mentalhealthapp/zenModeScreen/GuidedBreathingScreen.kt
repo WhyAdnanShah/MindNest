e923eb6e7ed83b58358fa75e2d5920b2e68510fc
Mohd Adnan <mohdadnan23742@gmail.com>
2025-07-24 23:56:28 +0530

    .

--- app/src/main/java/com/example/mentalhealthapp/zenModeScreen/GuidedBreathingScreen.kt
+++ app/src/main/java/com/example/mentalhealthapp/zenModeScreen/GuidedBreathingScreen.kt
@@ -2,33 +2,46 @@ package com.example.mentalhealthapp.zenModeScreen
 
 import android.util.Log
 import android.widget.Toast
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.FiniteAnimationSpec
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.unit.IntSize
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.example.mentalhealthapp.R
 import com.example.mentalhealthapp.moodScreen.TitleText
 import com.example.mentalhealthapp.navigation.BottomNavItem
 import kotlinx.coroutines.delay
+import java.nio.file.WatchEvent
 
 @Composable
 fun GuidedBreathingScreen(navController: NavHostController, title: String, rememberChipIndex: Int)  {
     val context = LocalContext.current
-    var initialSec by remember { mutableIntStateOf(4) }
+    var initialSec by remember { mutableIntStateOf(5) }
     var totalTime by remember { mutableIntStateOf(
         when (rememberChipIndex){
             0-> 60
@@ -57,13 +70,16 @@ fun GuidedBreathingScreen(navController: NavHostController, title: String, remem
         else -> 8
     }) }
 
-    when(totalTime){
-        60000-> Toast.makeText(context, "$totalTime $title", Toast.LENGTH_SHORT).show()
-        300000-> Toast.makeText(context, "$totalTime $title",Toast.LENGTH_SHORT).show()
-        600000-> Toast.makeText(context, "$totalTime $title",Toast.LENGTH_SHORT).show()
-        900000-> Toast.makeText(context, "$totalTime $title",Toast.LENGTH_SHORT).show()
-        1200000-> Toast.makeText(context, "$totalTime $title",Toast.LENGTH_SHORT).show()
-    }
+    val expandingCircle by animateDpAsState(
+        targetValue = when{
+            initialSec > 0 -> 100.dp
+            inhaleSec > 0 -> 200.dp
+            holdSec > 0 -> 150.dp
+            exhaleSec > 0 -> 400.dp
+            else -> 100.dp
+        }, animationSpec = tween(durationMillis = 1000)
+    )
+
 
     when(title){
         "Equal Breathing" -> {
@@ -162,5 +178,19 @@ fun GuidedBreathingScreen(navController: NavHostController, title: String, remem
                 else -> "Exhale"
             }, fontSize = 25.sp
         )
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ){
+        Card(
+            modifier = Modifier
+                .size(expandingCircle),
+            shape = RoundedCornerShape(400.dp),
+            colors = CardDefaults.cardColors((Color.Gray.copy(alpha = 0.3f)))
+        ){}
     }
 }
